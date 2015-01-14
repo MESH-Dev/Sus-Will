9e9aece9c618c449938b214406fbd2f64770c834
robbiesh <sherrard.rob@gmail.com>
2015-01-14 15:28:48 -0500

    footer fixes

--- wp-content/themes/sustainable-williamson/footer.php
+++ wp-content/themes/sustainable-williamson/footer.php
@@ -6,9 +6,11 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.png" />
         </div>
         <div class="footer-contact">
-          <p>184 East 2nd Ave. Suite 201<br/>
+          <p>City of Williamson<br/>
+          PO Box 1517<br/>
           Williamson, WV 25661</p>
-          <p><strong>Call:</strong> 1-304-236-5902<br/>
+          <p><strong>Call:</strong> 1-304-235-1510<br/>
+          <strong>Fax:</strong> 1-304-235-1516<br/>
           <strong>Hours:</strong> Monday - Friday, 9am - 5pm</p>
         </div>
       </div>
@@ -19,18 +21,22 @@
           <span>Affiliate Programs</span>
         </div>
         <div class="footer-body">
-          <p class="footer-link"><a href="">sustainablewilliamson.org »</a></p>
-          <p class="footer-link"><a href="">healthyselfies.org »</a></p>
-          <p class="footer-link"><a href="">mingocountydiabetescoalition.org »</a></p>
+          <p class="footer-link"><a href="http://williamsonhealthwellness.com">williamsonhealthwellness.com »</a></p>
+          <p class="footer-link"><a href="http://healthyselfies.org">healthyselfies.org »</a></p>
+          <p class="footer-link"><a href="http://mingocountydiabetescoalition.org">mingocountydiabetescoalition.org »</a></p>
         </div>
       </div>
     </div>
     <div class="four columns">
       <div class="footer-content">
         <div class="footer-headline">
-          <span>Social Media Feed</span>
+          <span>Follow Us</span>
         </div>
         <div class="footer-body">
+          <a href="https://www.facebook.com/SustainableWilliamsonWV" target="_blank"><div class="circle-icon"><i class="fa fa-facebook"></i></div></a>
+          <a href="https://twitter.com/suswilliamson" target="_blank"><div class="circle-icon"><i class="fa fa-twitter"></i></div></a>
+          <a href="http://vimeo.com/sustainablewilliamson" target="_blank"><div class="circle-icon"><i class="fa fa-vimeo-square"></i></div></a>
+          <a href="https://www.youtube.com/user/suswilliamson" target="_blank"><div class="circle-icon"><i class="fa fa-youtube"></i></div></a>
         </div>
       </div>
     </div>
